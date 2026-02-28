26ee5e6cfa9cc2c0b24f85c78c2bf5335a6f29ed
Eva Langerova <langerova_eva@yahoo.com>
2026-02-28 19:53:58 +0100

    create food item page

--- app/items/[id]/page.tsx
+++ app/items/[id]/page.tsx
@@ -4,6 +4,8 @@ import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import FoodItem from "@/models/FoodItem";
 import { FoodItemType } from "@/types/food";
+import deleteFood from "@/app/actions/deleteFood";
+import Link from "next/link";
 
 const FoodItemPage = async ({
   params,
@@ -29,15 +31,54 @@ const FoodItemPage = async ({
 
   if (!foodItem) {
     return (
-      <h1 className="text-center text-2xl font-bold mt-10">
-        Property No Found
-      </h1>
+      <h1 className="text-center text-2xl font-bold mt-10">Food Not Found</h1>
     );
   }
 
   return (
     <>
-      <h2>{foodItem.name}</h2>
+      <section>
+        <h2 className="text-xl border-b">
+          {foodItem.name.charAt(0).toUpperCase() + foodItem.name.slice(1)}
+        </h2>
+        {foodItem.details && <p>{foodItem.details}</p>}
+        <p>
+          Expires on{" "}
+          {new Date(foodItem.expirationDate).toLocaleDateString("en-GB", {
+            day: "numeric",
+            month: "long",
+            year: "numeric",
+          })}
+        </p>
+        <p>Category: {foodItem.category}</p>
+        <p>
+          Amount: {foodItem.quantity} {foodItem.unit}
+          {foodItem.quantity > 1 && "s"}
+        </p>
+        <p>Storage: {foodItem.storage}</p>
+
+        <div className="flex items-center gap-3">
+          <form action={deleteFood}>
+            <input
+              type="hidden"
+              name="foodId"
+              value={foodItem._id.toString()}
+            />
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-600 text-white font-bold cursor-pointer py-2 px-4 rounded-full focus:shadow-outline"
+            >
+              Delete
+            </button>
+          </form>
+          <Link
+            href={`/items/${foodItem._id}/edit`}
+            className="bg-red-600 hover:bg-red-700 text-white font-bold cursor-pointer py-2 px-4 rounded-full focus:shadow-outline"
+          >
+            Edit
+          </Link>
+        </div>
+      </section>
     </>
   );
 };
