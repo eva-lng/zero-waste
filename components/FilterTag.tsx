62f211568a2d6b53f7e3adb85509925714123559
Eva Langerova <langerova_eva@yahoo.com>
2026-03-08 21:54:43 +0100

    implement filter tag

--- components/FilterTag.tsx
+++ components/FilterTag.tsx
@@ -1,13 +1,23 @@
-const FilterTag = ({ label }: { label: string }) => {
-  const formattedLabel =
-    label === "soon"
-      ? "Expiring soon"
-      : label.charAt(0).toUpperCase() + label.slice(1);
+import { capitalize } from "@/lib/utils/food";
+import { FiX } from "react-icons/fi";
 
+const FilterTag = ({
+  param,
+  removeFilter,
+}: {
+  param: [string, string];
+  removeFilter: (type: string, value: string) => void;
+}) => {
   return (
-    <span className="inline-block bg-blue-100 text-blue-800 text-sm px-3 py-1 rounded-full">
-      {formattedLabel}
-    </span>
+    <div className="flex items-center gap-1 bg-blue-100 text-blue-800 text-sm px-3 py-1 rounded-full">
+      <span>{capitalize(param[1])}</span>
+      <button
+        className="cursor-pointer"
+        onClick={() => removeFilter(param[0], param[1])}
+      >
+        <FiX />
+      </button>
+    </div>
   );
 };
 
--- components/FiltersBar.tsx
+++ components/FiltersBar.tsx
@@ -1,10 +1,33 @@
 "use client";
 import { useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { MdFilterList } from "react-icons/md";
 import FilterPanel from "./FilterPanel";
+import FilterTag from "./FilterTag";
 
 const FiltersBar = () => {
   const [open, setOpen] = useState(false);
+
+  const params = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const allParamsArr = Array.from(params.entries());
+  console.log(allParamsArr);
+
+  function removeFilter(type: string, value: string): void {
+    const newParams = new URLSearchParams(params.toString());
+    const values = newParams.getAll(type);
+
+    if (values.includes(value)) {
+      const filtered = values.filter((v) => value !== v);
+      newParams.delete(type);
+      filtered.forEach((v) => newParams.append(type, v));
+    } else {
+      newParams.append(type, value);
+    }
+    router.replace(`${pathname}?${newParams.toString()}`);
+  }
 
   return (
     <>
@@ -16,6 +39,16 @@ const FiltersBar = () => {
         >
           <MdFilterList />
         </button>
+      </div>
+
+      <div className="flex flex-wrap gap-2">
+        {allParamsArr.map((param) => (
+          <FilterTag
+            key={`${param[0]}-${param[1]}`}
+            param={param}
+            removeFilter={removeFilter}
+          />
+        ))}
       </div>
 
       {open && <FilterPanel />}
