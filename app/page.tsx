8a2d5d0c9010de00bbb5fec49e81f140f4cfc120
Eva Langerova <langerova_eva@yahoo.com>
2026-07-10 12:13:54 +0200

    update home page content

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
 export default function Home() {
   return (
-    <>
-      <h2 className="text-3xl text-center">Homepage</h2>
-      <p className="text-center">Welcome! This is homepage</p>
-    </>
+    <section className="bg-primary-light grid md:grid-cols-2">
+      <div className="text-center md:text-start">
+        <p className="mb-6">LOGO</p>
+        <h1>Stop wasting food, start tracking it</h1>
+        <p>
+          Track your inventory, act before things expire, and see exactly how
+          much you're wasting each month.
+        </p>
+        <div>
+          <button className="btn-primary">Get started free</button>
+          <button className="btn-outline">Log in</button>
+        </div>
+      </div>
+      <div className="hidden md:flex items-center justify-center">
+        <div className="card"></div>
+      </div>
+    </section>
   );
 }
