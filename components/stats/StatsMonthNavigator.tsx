2c6999ae7a8984043c3813e74c6bdb397548750c
Eva Langerova <langerova_eva@yahoo.com>
2026-07-20 15:45:31 +0200

    add loading state StatsMonthNavigator

--- components/stats/StatsMonthNavigator.tsx
+++ components/stats/StatsMonthNavigator.tsx
@@ -1,7 +1,10 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { useTransition } from "react";
 import { MONTHS } from "@/lib/utils/constants";
 import { TbChevronLeft, TbChevronRight } from "react-icons/tb";
+import { Spinner } from "../ui/spinner";
+import { cn } from "@/lib/utils";
 
 const StatsMonthNavigator = ({
   year,
@@ -14,6 +17,8 @@ const StatsMonthNavigator = ({
   isFirst: boolean;
   isLast: boolean;
 }) => {
+  const [isPending, startTransition] = useTransition();
+
   const router = useRouter();
 
   const prevYear = month === 1 ? year - 1 : year;
@@ -21,30 +26,49 @@ const StatsMonthNavigator = ({
   const nextYear = month === 12 ? year + 1 : year;
   const nextMonth = month === 12 ? 1 : month + 1;
 
+  const handleNext = () => {
+    startTransition(() => {
+      router.push(
+        `/stats?month=${nextYear}-${String(nextMonth).padStart(2, "0")}`,
+      );
+    });
+  };
+
+  const handlePrev = () => {
+    startTransition(() => {
+      router.push(
+        `/stats?month=${prevYear}-${String(prevMonth).padStart(2, "0")}`,
+      );
+    });
+  };
+
   return (
     <div className="flex items-center justify-between md:gap-10">
       <button
-        disabled={isFirst}
-        className="w-7 h-7 flex items-center justify-center border rounded-md hover:bg-muted cursor-pointer disabled:invisible"
-        onClick={() =>
-          router.push(
-            `/stats?month=${prevYear}-${String(prevMonth).padStart(2, "0")}`,
-          )
-        }
+        disabled={isFirst || isPending}
+        className={cn(
+          "w-7 h-7 flex items-center justify-center border rounded-md hover:bg-muted cursor-pointer disabled:opacity-60 disabled:pointer-events-none",
+          isFirst && "invisible",
+        )}
+        // "w-7 h-7 flex items-center justify-center border rounded-md hover:bg-muted cursor-pointer disabled:invisible"
+        onClick={handlePrev}
       >
         <TbChevronLeft className="text-muted-foreground" />
       </button>
-      <span className="text-[15px] font-medium">
-        {MONTHS[month - 1]} {year}
-      </span>
+      <div className="relative flex items-center justify-center">
+        {isPending && <Spinner className="absolute -left-6" />}
+        <span className="text-[15px] font-medium">
+          {MONTHS[month - 1]} {year}
+        </span>
+      </div>
       <button
-        disabled={isLast}
-        className="w-7 h-7 flex items-center justify-center border rounded-md hover:bg-muted cursor-pointer disabled:invisible"
-        onClick={() =>
-          router.push(
-            `/stats?month=${nextYear}-${String(nextMonth).padStart(2, "0")}`,
-          )
-        }
+        disabled={isLast || isPending}
+        className={cn(
+          "w-7 h-7 flex items-center justify-center border rounded-md hover:bg-muted cursor-pointer disabled:opacity-60 disabled:pointer-events-none",
+          isLast && "invisible",
+        )}
+        // {`w-7 h-7 flex items-center justify-center border rounded-md hover:bg-muted cursor-pointer disabled:invisible ${isPending && "opacity-60 pointer-events-none disabled"}`}
+        onClick={handleNext}
       >
         <TbChevronRight className="text-muted-foreground" />
       </button>
